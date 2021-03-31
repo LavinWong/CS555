876f26bf8a5833f6c17440563033fb242ecac0e6
Qi Zhao <qiblaqi@126.com>
2021-03-31 03:47:10 -0400

    update us12_test() print test info

--- src/main.cpp
+++ src/main.cpp
@@ -160,6 +160,11 @@ class Cppunit_tests: public Cppunit {
         Repository repo;
         std::vector<std::string> result = {"NA"};
         std::vector<std::string> result1 = repo.us12();
+        std::cout<<"-----US12_test------"<<std::endl;
+        for (auto str: result1){
+            std::cout<<str<<std::endl;
+         }
+         std::cout<<"-----US12_test_END------"<<std::endl;
         CHECKT(result1 == result);
     }
     
