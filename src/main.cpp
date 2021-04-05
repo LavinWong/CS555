b1bb1ade4e7a329da8257395b83c71dbd4f3f864
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2021-04-05 12:12:58 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -213,20 +213,20 @@ class Cppunit_tests: public Cppunit {
     void us23_test(){
         //todo
         Repository repo;
-        std::vector<std::string> result = {"@I1@", "@I2@"};
+        std::vector<std::string> result = {"@I1@", "@I2@","@I3@", "@I5@", "@I6@","@I7@", "@I8@","@I9@", "@I10@","@I11@", "@I12@","@I13@", "@I14@","@I15@", "@I16@","@I17@", "@I18@","@I19@", "@I20@","@I21@", "@I22@","@I23@", "@I24@","@I25@"};
         std::vector<std::string> result2 = repo.us23();
         std::vector<std::string> result3 = result;
-         std::cout<<"-----US22_test------"<<std::endl;
+         std::cout<<"-----US23_test------"<<std::endl;
          for (auto str: result2){
              std::cout<<str<<std::endl;
           }
-          std::cout<<"-----US22_test_END------"<<std::endl;
-          std::cout<<"-----US22_result------"<<std::endl;
+          std::cout<<"-----US23_test_END------"<<std::endl;
+          std::cout<<"-----US23_result------"<<std::endl;
           for (auto str: result3){
               std::cout<<str<<std::endl;
           }
-          std::cout<<"-----US22_result_END------"<<std::endl;
-        CHECKT(repo.us23() != result);
+          std::cout<<"-----US23_result_END------"<<std::endl;
+        CHECKT(repo.us23() == result);
     }
     
     void us27_test(){
