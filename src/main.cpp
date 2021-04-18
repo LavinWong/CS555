0c119ef49d972437b69e26ed3372bb8e5cdf734c
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2021-04-18 22:57:44 +0800

    Add test35

--- src/main.cpp
+++ src/main.cpp
@@ -269,6 +269,21 @@ class Cppunit_tests: public Cppunit {
     
     void us35_test(){
         //todo
+        Repository repo;
+        std::vector<std::string> result = {"@I1@", "@I2@","@I3@", "@I5@", "@I6@","@I7@", "@I8@","@I9@", "@I10@","@I11@", "@I12@","@I13@", "@I14@","@I15@", "@I16@","@I17@", "@I18@","@I19@", "@I20@","@I21@", "@I22@","@I23@", "@I24@","@I25@"};
+        std::vector<std::string> result2 = repo.us35();
+        std::vector<std::string> result3 = result;
+         std::cout<<"-----US35_test------"<<std::endl;
+        for (auto str: result2){
+             std::cout<<str<<std::endl;
+          }
+          std::cout<<"-----US35_test_END------"<<std::endl;
+          std::cout<<"-----US35_result------"<<std::endl;
+          for (auto str: result3){
+              std::cout<<str<<std::endl;
+          }
+          std::cout<<"-----US35_result_END------"<<std::endl;
+        CHECKT(repo.us35() == result);
     }
     
     void us36_test(){
