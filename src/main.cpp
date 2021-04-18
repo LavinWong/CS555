39222b76127fe80691a70632b0c42a712685a7a9
Zichong Wong <72814415+LavinWong@users.noreply.github.com>
2021-04-18 23:07:32 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -270,7 +270,7 @@ class Cppunit_tests: public Cppunit {
     void us35_test(){
         //todo
         Repository repo;
-        std::vector<std::string> result = {"@I1@", "@I2@","@I3@", "@I5@", "@I6@","@I7@", "@I8@","@I9@", "@I10@","@I11@", "@I12@","@I13@", "@I14@","@I15@", "@I16@","@I17@", "@I18@","@I19@", "@I20@","@I21@", "@I22@","@I23@", "@I24@","@I25@"};
+        std::vector<std::string> result = {"@I1@"};
         std::vector<std::string> result2 = repo.us35();
         std::vector<std::string> result3 = result;
          std::cout<<"-----US35_test------"<<std::endl;
